4a22af996f8b49d74d8b0ccaf6e9ecf35c9520dc
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-05 11:57:47 +0400

    Bir string arrayinin icersinde uzunlugu 5 den boyuk olan sozleri ekrana cixardan bir alqoritm yazin

---
+++ loop 3/loop 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace loop_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "Ilkin", "Ferid", "Dunyamali", "Sahile" };
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i].Length>5)
+                {
+                    Console.WriteLine(name[i]);
+
+                }
+
+            }
+        }
+    }
+}
