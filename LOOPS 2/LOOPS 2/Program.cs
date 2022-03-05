379c58826574c04ccc61d0c619131baa5b18563a
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-05 11:43:18 +0400

    Verilmiş tam müsbət n ədədinin rəqəmlərinin cəmini tapan alqoritm

---
+++ LOOPS 2/LOOPS 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LOOPS_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+          while (number > 0)
+            {
+                sum = sum + number % 10;
+                number /= 10;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
